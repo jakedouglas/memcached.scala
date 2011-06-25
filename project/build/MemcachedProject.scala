4cf19133d067fe210f178338f916581adebd44fa
Jake Douglas <jakecdouglas@gmail.com>
2011-06-25 11:27:22 -0700

    2.8.1

--- project/build/MemcachedProject.scala
+++ project/build/MemcachedProject.scala
@@ -6,6 +6,6 @@ import java.lang.ProcessBuilder
 class MemcachedProject(info: ProjectInfo) extends DefaultProject(info) {
   val specsRepo = "Specs Repo" at "http://nexus.scala-tools.org/content/repositories/snapshots"
 
-  val specs   = "org.scala-tools.testing" %% "specs" % "1.6.5"
+  val specs   = "org.scala-tools.testing" %% "specs" % "1.6.6"
   val mockito = "org.mockito" % "mockito-all" % "1.8.5"
 }
